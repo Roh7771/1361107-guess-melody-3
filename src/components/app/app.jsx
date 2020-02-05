2b84c034596cf06645398a158297c991f88c5ed9
Belov <belov.rom-77@yandex.ru>
2020-02-05 19:44:33 +0300

    Добавлены первые компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from "React";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
+  return <WelcomeScreen errorsCount={errorsCount} />;
+};
+
+export default App;
