639cf10623d56c465a57f7a43872b1b655e7c01a
Belov <belov.rom-77@yandex.ru>
2020-02-07 06:15:10 +0300

    Добавлена валидация пропсов для компонентов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,13 @@
-import React from "React";
+import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import PropTypes from "prop-types";
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {errorsCount} = props;
-
+const App = ({errorsCount}) => {
   return <WelcomeScreen errorsCount={errorsCount} />;
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
 };
 
 export default App;
