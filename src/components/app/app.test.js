93d484d3fdb231fd387640c3d90918e66b87b7bd
Roman Belov <belov.rom-77@yandex.ru>
2020-02-08 08:39:55 +0300

    Название тестов изменены

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app";
 
-it(`<Albums /> should render Cinderella`, () => {
+it(`Render App`, () => {
   const tree = renderer
     .create(<App
       errorsCount={4}
--- src/components/welcome-screen/welcome-screen.test.jsx
+++ src/components/welcome-screen/welcome-screen.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import WelcomeScreen from "./welcome-screen";
 
-it(`<Albums /> should render Cinderella`, () => {
+it(`Should WelcomeScreen render correctly`, () => {
   const tree = renderer
     .create(<WelcomeScreen
       errorsCount={5}
