86b2037e3eec78cbb96e0230fd8aa3a4dbcc25b1
Belov <belov.rom-77@yandex.ru>
2020-02-11 07:31:56 +0300

    Добавлены моки с вопросами

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import {Settings} from "./mocks/mock.js";
+import Settings from "./mocks/settings";
 
 ReactDOM.render(<App errorsCount={Settings.ERRORS_COUNT} />, document.getElementById(`root`)
 );
--- src/mocks/mock.js
+++
@@ -1,3 +0,0 @@
-export const Settings = {
-  ERRORS_COUNT: 3
-};
