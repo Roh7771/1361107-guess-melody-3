1798ebb2276f8e85d7c2f72d9843fe6664d7e14f
Belov <belov.rom-77@yandex.ru>
2020-02-18 06:48:40 +0300

    Моки с вопросами обновлены

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -6,39 +6,39 @@ export default [
     type: `genre`,
     genre: `rock`,
     answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/2/26/John_Hoopie_-_Don%27t_You_Know.ogg`,
       genre: `rock`,
       id: id++,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4d/2009-05-29MedBoogie.ogg`,
       genre: `blues`,
       id: id++,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/2/21/03_Turning_Points.ogg`,
       genre: `jazz`,
       id: id++,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/d7/Caleb_Baker_-_Crush_Song.ogg`,
       genre: `rock`,
       id: id++,
     }],
   }, {
     type: `artist`,
     song: {
-      artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      artist: `Mozart`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/d6/KV.265_12_Variations_on_Ah_vous_dirai-je%2C_Maman_Mozart_JMC%2C_Han.ogg`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `John Snow`,
+      artist: `Mozart`,
       id: id++,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jack Daniels`,
+      artist: `Bach`,
       id: id++,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jim Beam`,
+      artist: `Beethoven`,
       id: id++,
     }],
   }
