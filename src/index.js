9a08d93fa4c6ba0450ef2c62b664ea4ad8c65fa8
Belov <belov.rom-77@yandex.ru>
2020-02-21 07:03:59 +0300

    Установлен Redux и произведена первоначальная настройка

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
 import App from "./components/app/app.jsx";
 import Settings from "./mocks/settings";
 import Questions from "./mocks/questions";
+import reducer from "./reducer.js";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App questions={Questions} errorsCount={Settings.ERRORS_COUNT} />, document.getElementById(`root`)
+const store = createStore(reducer);
+
+const AppWrapper = <Provider store={store}><App questions={Questions} errorsCount={Settings.ERRORS_COUNT} /></Provider>;
+
+ReactDOM.render(AppWrapper, document.getElementById(`root`)
 );
