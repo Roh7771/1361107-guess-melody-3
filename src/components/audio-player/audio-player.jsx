c2a474e54c6062f9c03b0ba84c202e552dfcf8c2
Belov <belov.rom-77@yandex.ru>
2020-02-25 08:03:01 +0300

    Состояние плеера вынесено в  HOC

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,64 +1,9 @@
-import React, {PureComponent, createRef} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 
 class AudioPlayer extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      progress: 0,
-      isLoading: true,
-      isPlaying: props.isPlaying,
-    };
-    this._audioRef = createRef();
-  }
-
-  componentDidMount() {
-    const {src} = this.props;
-    const audio = this._audioRef.current;
-
-    audio.src = src;
-
-    audio.oncanplaythrough = () => this.setState({
-      isLoading: false,
-    });
-
-    audio.onplay = () => {
-      this.setState({
-        isPlaying: true,
-      });
-    };
-
-    audio.onpause = () => this.setState({
-      isPlaying: false,
-    });
-
-    audio.ontimeupdate = () => this.setState({
-      progress: audio.currentTime
-    });
-  }
-
-  componentDidUpdate() {
-    if (this.props.isPlaying) {
-      this._audioRef.current.play();
-    }
-    if (!this.props.isPlaying) {
-      this._audioRef.current.pause();
-    }
-  }
-
-  componentWillUnmount() {
-    const audio = this._audioRef.current;
-
-    audio.oncanplaythrough = null;
-    audio.onplay = null;
-    audio.onpause = null;
-    audio.ontimeupdate = null;
-    audio.src = ``;
-  }
-
   render() {
-    const {isPlaying, isLoading} = this.state;
-    const {onPlayButtonClick} = this.props;
+    const {onPlayButtonClick, isPlaying, isLoading, children} = this.props;
     return (
       <div className="track">
         <button
@@ -66,17 +11,12 @@ class AudioPlayer extends PureComponent {
           type="button"
           disabled={isLoading}
           onClick={() => {
-            this.setState((prevState) => {
-              return {
-                isPlaying: !prevState.isPlaying,
-              };
-            });
             onPlayButtonClick();
           }}
         >
         </button>
         <div className="track__status">
-          <audio ref={this._audioRef} />
+          {children}
         </div>
       </div>
     );
@@ -85,8 +25,12 @@ class AudioPlayer extends PureComponent {
 
 AudioPlayer.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
-  src: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   onPlayButtonClick: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
 };
 
 export default AudioPlayer;
