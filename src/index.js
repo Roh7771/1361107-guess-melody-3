e2c8b84aac806f4300cf48a3b9a14de6627f6779
Belov <belov.rom-77@yandex.ru>
2020-03-03 08:14:06 +0300

    Redux-thunk подключен

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import App from "./components/app/app.jsx";
 import {reducer} from "./reducer.js";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {Operation as DataOperation} from "./reducer.js";
+import {createAPI} from "./api.js";
+
+const api = createAPI(() => {});
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
+
+store.dispatch(DataOperation.loadQuestions());
 
 const AppWrapper = <Provider store={store}><App/></Provider>;
 
